8ad185f21db9242a05a96c427101ed3d22552802
Šrámek <jakubsramek2006@gmail.com>
2025-12-10 22:26:58 +0100

    Frontend updated(separated into app.js,index,html,style.css

---
+++ public/app.js
@@ -0,0 +1,40 @@
+async function sendOrder() {
+    const shopAddress = document.getElementById("shopAddress").value;
+    const userAddress = document.getElementById("userAddress").value;
+    const items = document.getElementById("items").value.split(",").map(t => t.trim());
+
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "<p>Odesílám objednávku...</p>";
+
+    try {
+        const res = await fetch("http://localhost:3000/api/order/create", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                shopAddress,
+                userAddress,
+                items
+            })
+        });
+
+        const data = await res.json();
+
+        if (data.error) {
+            resultDiv.innerHTML = `
+            <div class="result">
+                <b>Chyba:</b> ${data.error}
+            </div>`;
+            return;
+        }
+
+        resultDiv.innerHTML = `
+        <div class="result">
+            <h3>Objednávka vytvořena!</h3>
+            <p><b>Vzdálenost:</b> ${data.distance} km</p>
+            <p><b>Zvolené auto:</b> ${data.chosenCar.name}</p>
+        </div>`;
+    }
+    catch (e) {
+        resultDiv.innerHTML = `<div class="result"><b>Chyba připojení k backendu.</b></div>`;
+    }
+}
