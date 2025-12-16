b6676fa92eb5b3d6d553180aadd3b480e8024138
Šrámek <jakubsramek2006@gmail.com>
2025-12-16 19:27:22 +0100

    Updated

--- public/script.js
+++ public/script.js
@@ -1,14 +1,33 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-const cartCountSpan = document.getElementById("cartCount");
-if (cartCountSpan) {
-  cartCountSpan.textContent = cart.length;
-}
+const PRODUCTS = {
+  "Židle": {
+    name: "Židle",
+    dimensions: { width: 45, length: 45, height: 90 },
+    weight: 6
+  },
+  "Sedačka": {
+    name: "Sedačka",
+    dimensions: { width: 200, length: 90, height: 85 },
+    weight: 60
+  },
+  "Postel": {
+    name: "Postel",
+    dimensions: { width: 180, length: 200, height: 50 },
+    weight: 80
+  },
+  "Skříň": {
+    name: "Skříň",
+    dimensions: { width: 120, length: 60, height: 200 },
+    weight: 90
+  }
+};
 
-function addToCart(item) {
-  cart.push(item);
+function addToCart(productName) {
+  const product = PRODUCTS[productName];
+
+  cart.push(product);
   localStorage.setItem("cart", JSON.stringify(cart));
-  if (cartCountSpan) cartCountSpan.textContent = cart.length;
 }
 
 function renderCart() {
@@ -16,10 +35,17 @@ function renderCart() {
   if (!cartItemsDiv) return;
 
   cartItemsDiv.innerHTML = "";
-  cart.forEach((item, index) => {
+
+  cart.forEach((item, i) => {
     const div = document.createElement("div");
     div.className = "cart-item";
-    div.textContent = `${index + 1}. ${item}`;
+    div.innerHTML = `
+      <b>${i + 1}. ${item.name}</b><br>
+      ${item.dimensions.width} ×
+      ${item.dimensions.length} ×
+      ${item.dimensions.height} cm<br>
+      ${item.weight} kg
+    `;
     cartItemsDiv.appendChild(div);
   });
 }
